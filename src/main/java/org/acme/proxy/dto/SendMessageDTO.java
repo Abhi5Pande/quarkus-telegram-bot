773a558fa6bb9c70971ad1eca160ca9bdf7b3164
Abhijit <abhi.pachpande@gmail.com>
2024-05-01 14:53:11 +0530

    added reflections and removed telegram dependency

--- src/main/java/org/acme/proxy/dto/SendMessageDTO.java
+++ src/main/java/org/acme/proxy/dto/SendMessageDTO.java
@@ -1,5 +1,9 @@
 package org.acme.proxy.dto;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@RegisterForReflection
 public class SendMessageDTO {
     public String chat_id;
     public String text;
--- src/main/java/org/acme/resource/dto/MessageReqDTO.java
+++ src/main/java/org/acme/resource/dto/MessageReqDTO.java
@@ -1,10 +1,12 @@
 package org.acme.resource.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
+@RegisterForReflection
 public class MessageReqDTO {
     private String name;
     private String email;
