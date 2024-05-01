d868a7b78c6b6546402c315c73261d9d3712002e
Abhijit <67785307+Abhi5Pande@users.noreply.github.com>
2024-05-01 14:56:21 +0530

    Update GreetingResource.java

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello abhijit from Quarkus REST";
+        return "Github action try";
     }
 }
